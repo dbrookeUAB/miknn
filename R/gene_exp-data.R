576f701266a68b8f01f4f85e579496183f05fc7d
Dewey Brooke <dbrooke@uab.edu>
2019-08-02 06:07:57 -0500

    Created a data file that loads with the package. Removed junk and consolidated into one file.

---
+++ R/gene_exp-data.R
@@ -0,0 +1,12 @@
+#' Gene Expression Dataset for miknn
+#' @docType data
+#'
+#' @usage data(gene_exp)
+#'
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data("gene_exp", package = "miknn")
+
+"gene_exp"
