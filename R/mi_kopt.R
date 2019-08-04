2afa4e5c1cde73cee2a6290ebf10bed0ba15b36a
Dewey Brooke <dbrooke@uab.edu>
2019-08-04 10:07:51 -0500

    plot displays ci and mi_kopt returns optimal k

--- R/mi_kopt.R
+++ R/mi_kopt.R
@@ -14,17 +14,17 @@
 #' library(miknn)
 #' 
 #' # determining I for a series of k
-#' k.series <- 3:40
-#' mi_kopt(iris, "Species","Sepal.Length", k.series, global = FALSE)
+#' mi_kopt(iris, "Species","Sepal.Length", global = FALSE)
 #' 
-mi_kopt <- function(dt, var.d, var.c, k.series, global = TRUE){
+mi_kopt <- function(dt, var.d, var.c,  global = TRUE, returnK = FALSE){
   suppressWarnings({
+    
+    k.series <- c(1:dt[,.N, var.d][which.min(N),N-1])
     pb <- progress::progress_bar$new(
       format = ":percent k = :k [:bar] :elapsed | eta: :eta",
       total = length(k.series),    # 100 
       width = 60, clear = TRUE
     )
-    
     if(global){
       result <- data.table(k = k.series, 
                  I = sapply(k.series, function(x){
@@ -32,6 +32,7 @@ mi_kopt <- function(dt, var.d, var.c, k.series, global = TRUE){
                    mi_knn(dt, var.d,var.c, x, global = TRUE, FORCE = TRUE)
                  })
       )[!is.na(I)]
+      result$se <- stats::predict(stats::loess(I~k, result, span = 0.5), se = TRUE)$se
     } else {
       result <-data.table(k = k.series, 
                  do.call(rbind,
@@ -46,7 +47,13 @@ mi_kopt <- function(dt, var.d, var.c, k.series, global = TRUE){
     } 
   })
   class(result) <- c("mi_kopt",class(result))
-  return(result)
+  
+  if(!returnK&global){
+    return(result)
+  } else {
+    result[,I/sqrt(se),k][order(V1, decreasing = TRUE),k][1]
+  }
+  
 }
 
 
@@ -61,10 +68,21 @@ mi_kopt <- function(dt, var.d, var.c, k.series, global = TRUE){
 #'
 #' @examples
 plot.mi_kopt <- function(x,...){
-  model <- stats::predict(stats::loess(I~k, x, span = 0.8), se = TRUE)
-  graphics::plot(I~k, x,...)
+  model <- stats::predict(stats::loess(I~k, x, span = 0.5), se = TRUE)
   
-  graphics::lines(model$fit)
-  graphics::lines(model$fit - qt(0.995,model$df)*model$se, lty=2)
-  graphics::lines(model$fit + qt(0.995,model$df)*model$se, lty=2)
+  ci <- data.frame(
+    k = x$k,
+    fit = model$fit,
+    upper =model$fit + qt(0.995,model$df)*model$se,
+    lower = model$fit - qt(0.995,model$df)*model$se
+    )
+  
+  ylim <- c(min(ci$lower)-min(ci$lower)*0.025, max(ci$upper)+max(ci$upper)*0.025)
+  
+  graphics::plot(I~k, x,pch = 19,ylim = ylim, ...)
+  
+  graphics::lines(fit~k,ci, col = "black", lty = 1)
+  graphics::lines(upper~k, ci, col = "red", lty = 4)
+  graphics::lines(lower~k, ci, col = "red", lty = 4)
+
 }
